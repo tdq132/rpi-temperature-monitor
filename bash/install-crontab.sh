a8b5a7d5106cf655940feed8964605d2017e8718
Thomas Quirke <tq@outlook.co.nz>
2021-09-07 21:16:46 +1200

    Fixes install-crontab.sh

--- bash/install-crontab.sh
+++ bash/install-crontab.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-set -e
 
+# This script will dump your current crontab to file
+#  then append a new crontab entry to run the rpi-temperature-monitor
+#  script. It will run every 5 minutes with the default schedule below.
 
 # write out the currrent crontab
 crontab -l > cronlist
@@ -11,3 +13,4 @@ echo "*/5 * * * * /opt/rpi-temperature-monitor/bash/run.sh" >> cronlist
 # install new cron file
 crontab cronlist
 rm cronlist
+
